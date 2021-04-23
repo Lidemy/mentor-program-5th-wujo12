64bef5eb9bfdd45cd9cfd61e5c17f06ccf481be1
wujo <wujack1219@yahoo.com.tw>
2021-04-23 21:52:14 +0800

    week2finished

--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,10 +1,28 @@
 function join(arr, concatStr) {
-  
-}
+var a=[]
+  if (arr.length === 1){
+    return arr
+  }
+  else  {
+    for(i=0;i<arr.length;i++)
+      if (i ===arr.length-1){
+      a+=arr[i]
+      }
+      else{
+      a+=arr[i]+concatStr
+      } 
+    } 
+   return a 
+} 
+
 
 function repeat(str, times) {
-  
+  var word=''
+    for(i=1;i<times;i++){    
+      word+=str
+    }return word
 }
 
-console.log(join(['a'], '!'));
+
+console.log(join(['a','b'], '!'));
 console.log(repeat('a', 5));
